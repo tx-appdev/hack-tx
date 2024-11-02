bc712f5c636d72c65710947be2a069af2ce13a9e
Nathan Foo <ntfoo27@outlook.com>
2024-11-02 17:00:14 -0500

    Implement lenis

--- client-next/app/layout.tsx
+++ client-next/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import SmoothScroll from "./components/ScrollTrigger";
 
 export const metadata: Metadata = {
   title: "Hack TX 2024",
@@ -12,10 +13,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
+    <SmoothScroll>
+      <html lang="en">
+        <body>
+          {children}
+        </body>
+      </html>
+    </SmoothScroll>
   );
 }

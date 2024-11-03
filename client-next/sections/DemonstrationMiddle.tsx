fdac8150800e5f87f52d249eb1366e1335f2ec62
Nathan Foo <ntfoo27@outlook.com>
2024-11-03 04:00:01 -0600

    update descriptions

--- client-next/sections/DemonstrationMiddle.tsx
+++ client-next/sections/DemonstrationMiddle.tsx
@@ -8,7 +8,8 @@ const DemonstrationMiddle = () => {
 
                 <h2 className="font-[SuperLegend] text-[1.5em] text-[--body] text-center pl-[150px]">
                     Get a live preview of your new assets in action<br />
-                    through a generated theme window.<br />
+                    through a generated theme window. See your main<br />
+                    character in a custom retro kingdom!
                 </h2>
             </div>
             <img src='pixel_art.png' width="500" height="500" className="w-[1500px] h-[500px] scale-x-[-1]"/>

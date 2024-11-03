202f0f54a186f70b4e3c91c23d681e674d0b974a
Nathan Foo <ntfoo27@outlook.com>
2024-11-03 03:45:42 -0600

    update descriptions

--- client-next/sections/DemonstrationMiddle.tsx
+++ client-next/sections/DemonstrationMiddle.tsx
@@ -3,13 +3,12 @@ const DemonstrationMiddle = () => {
         <div className="demonstration min-h-[100vh] my-0 mx-auto flex items-left justify-center">
             <div>
                 <h1 className='font-[SuperLegend] text-[3em] text-[--body] text-center pb-[50px] pl-[150px]'>
-                    <b>Create retro style textures in seconds</b>
+                    <b>Visualize your next indie game</b>
                 </h1>
 
                 <h2 className="font-[SuperLegend] text-[1.5em] text-[--body] text-center pl-[150px]">
-                    Reimagine the lost art of retro gaming through our <br />
-                    easy to use AI tool. Simply navigate to the <b>generate</b> <br />
-                    tab and enter in a prompt and wait for your image to be created.
+                    Get a live preview of your new assets in action<br />
+                    through a generated theme window.<br />
                 </h2>
             </div>
             <img src='pixel_art.png' width="500" height="500" className="w-[1500px] h-[500px] scale-x-[-1]"/>

58ec8a0c037696aefde16a402cb8f3d189b946ef
Dylan <dylanrn2006@gmail.com>
2024-11-03 06:13:02 -0600

    final merge branch

--- backend/fetch.py
+++ backend/fetch.py
@@ -14,23 +14,7 @@ def receive_data():
     assets = prompt_generator.extract_keywords(data.get("gameIdea"), int(data.get("numberOfAssets")))
     print(assets)
     stability.createImage(assets)
-    images_data = []
-    
-    for i in range(0, int(data.get("numberOfAssets"))):
-        image_file = f'generated_image_{i}.png'
-        with open(image_file, 'rb') as img_file:
-        # Encode image to Base64
-            encoded_string = base64.b64encode(img_file.read()).decode('utf-8')
-         # Append image data to the list
-        images_data.append({
-        'file_name': image_file,  # Original path or file name
-        'image_data': encoded_string  # Base64 encoded image data
-        })
-        
-    json_data = {
-        'images': images_data
-    }
-    print(jsonify(json_data))
+    json_data = []
     return jsonify(json_data)
 
 

e1a6b630e4a1f06208a827ec3cbee0e054a5996a
Nathan Foo <ntfoo27@outlook.com>
2024-11-03 01:30:23 -0600

    add content to about page

--- client-next/sections/About.tsx
+++ client-next/sections/About.tsx
@@ -17,32 +17,38 @@ const About = () => {
                 <h2 className="font-[Karmatic] text-[3em] text-[--scroll]">
                     Inspiration
                 </h2>
-                <p className="font-[SuperLegend] text-[1.5em] text-[--body] py-4">
-                    Paragraph 1 - Here is some random text to make it seem like we did some super cool stuff
+                <p className="font-[SuperLegend] text-[1.5em] py-8 text-[--body]">
+                    Our inspiration for this project stems from our desire to make a positive impact in the gaming community. We hope to support indie game developers with this AI tool and enable them to continue doing what they love!
                 </p>
                 <h2 className="font-[Karmatic] text-[3em] text-[--scroll]">
                     What It Does
                 </h2>
-                <p className="font-[SuperLegend] text-[1.5em] py-4 text-[--body]">
-                    Paragraph 2
+                <p className="font-[SuperLegend] text-[1.5em] py-8 text-[--body]">
+                    Indie Studio is designed to empower indie developers to create unique and visually appealing games.     This tool provides developers with AI-generated assets including themes, sprites, and color palettes tailored to their specific needs, and is essential to kickstart new indie applications.
                 </p>
                 <h2 className="font-[Karmatic] text-[3em] text-[--scroll]">
                     What We Learned
                 </h2>
-                <p className="font-[SuperLegend] text-[1.5em] py-4 text-[--body]">
-                    Paragraph 3
+                <p className="font-[SuperLegend] text-[1.5em] py-8 text-[--body]">
+                    Through this project, we learned to utilize AI and stable diffusion for image generation in a full stack environment. We also learned to implement Python with a Next.js frontend to access powerful tools for backend development. Finally, we improved our front end design skills with horizontal scrolling, falling petal effects, and an aesthetic color palette.
                 </p>
                 <h2 className="font-[Karmatic] text-[3em] text-[--scroll]">
                     How We Built It
                 </h2>
-                <p className="font-[SuperLegend] text-[1.5em] py-4 text-[--body]">
-                    Paragraph 4
+                <p className="font-[SuperLegend] text-[1.5em] py-8 text-[--body]">
+                    We opted to build our web application using React and Next.js for fast loading speeds and static site generation.
+                    <br /><br />
+                    We developed three pages including a user friendly landing page, about section, and generative interface for asset creation, and used Flask to connect our application to our Python backend.
+                    <br /><br />
+                    We used Python to implement stable diffusion and theme color generators with AI, and received generation directions from a text input provided by the user.
                 </p>
                 <h2 className="font-[Karmatic] text-[3em] text-[--scroll]">
                     Challenges We Faced
                 </h2>
-                <p className="font-[SuperLegend] text-[1.5em] py-4 text-[--body]">
-                    Paragraph 5
+                <p className="font-[SuperLegend] text-[1.5em] py-8 text-[--body]">
+                    One challenge we faced was implementing stable diffusion into our project. The main problem we faced was the stable diffusion program taking a long time to generate, but we made our best attempt to increase load speeds.
+                    <br /><br />
+                    Another challenge we faced was designing a clean user experience that fit well with the hackathon theme. We went through several theme ideas before deciding on the current version and encountered problems with implementing several design ideas, but in the end we finally came up with a version that we were satisfied with.
                 </p>
                 <div className="py-10"></div>
             </div>
